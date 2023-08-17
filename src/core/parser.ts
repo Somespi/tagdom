2e0021605c4faf24678164499719e39939544b69
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-17 09:54:04 +0400

    declare parser

---
+++ src/core/parser.ts
@@ -0,0 +1,10 @@
+import { Token } from "./tokentype";
+
+const parser = (tokens: Token[]) => {
+    let currentok= 0;
+    while (currentok < tokens.length) {
+
+        
+        currentok++;
+    }
+}
