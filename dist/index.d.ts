a305e1eee9f902923654a622603241c86da3bc2c
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-17 22:01:38 +0400

    re-compiling ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-export { default as tag } from "./generator/backtick";
+export {};
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.tag = void 0;
-var backtick_1 = require("./generator/backtick");
-Object.defineProperty(exports, "tag", { enumerable: true, get: function () { return __importDefault(backtick_1).default; } });
+const backtick_1 = __importDefault(require("./generator/backtick"));
+module.exports = backtick_1.default;
