4589285f83443b3b03c945ea2f2d53e9f5e0bdb0
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-17 21:35:06 +0400

    implement builder

--- src/generator/builder.ts
+++ src/generator/builder.ts
@@ -1,5 +1,14 @@
-import { Tag } from "../core/tagging"
+import { tokenize, parse } from "../core/";
+import { Tag } from "../core/tagging";
 
-const build  = (tags: Tag[]): string => {
-    return ''
+const build  = (tag: Tag): HTMLElement => {
+    let element: HTMLElement  = document.createElement(tag.name);
+    element.innerHTML = tag.value;
+    tag.children.forEach(child => {
+        element.appendChild(build(child));
+    })
+
+    return element;
+
 }
+export default build
